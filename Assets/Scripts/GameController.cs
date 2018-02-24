34f782af6cbca2e4ba231e49c41d78980cede331
Dennis Carlstrom <lazy@lazymammal.com>
2018-02-24 16:00:43 -0500

    disable wave height displacement

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        // save any game data here
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPaused = true;
+#else
+        Application.PauseGame();
+#endif
+
+    }
+
+    public void QuitGame()
+    {
+        // save any game data here
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+
+    }
+}
---
+++ Assets/Scripts/WaveHeightPeep.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class WaveHeightPeep : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+		float distance = 5f;
+		Ray ray = new Ray(Camera.main.transform.position + Vector3.up * distance, Vector3.down);
+		RaycastHit hit;
+
+        Physics.Raycast(ray, out hit, distance * 2f, 4);
+
+		/*
+		Debug.Log("=====");
+		Debug.Log(ray.origin);
+		Debug.Log(hit.point);
+		*/
+    }
+}
