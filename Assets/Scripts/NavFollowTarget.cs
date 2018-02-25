cd39bb0fbab31ebe79885d4b1522eb4d152eb6ea
Dennis Carlstrom <lazy@lazymammal.com>
2018-02-25 11:12:17 -0500

    Skeletor chases player

---
+++ Assets/Scripts/NavFollowTarget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavFollowTarget : MonoBehaviour
+{
+    public Transform target;
+    Vector3 destination;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        destination = agent.destination;
+        if (!target)
+            target = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(destination, target.position) > 1.0f)
+        {
+            destination = target.position;
+            agent.SetDestination(destination);
+        }
+    }
+}
