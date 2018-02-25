f2c8e089af89e7a01612a96dc68b07b047d38e87
Dennis Carlstrom <lazy@lazymammal.com>
2018-02-25 09:33:16 -0500

    efficiency

--- Assets/Scripts/MoveIt.cs
+++ Assets/Scripts/MoveIt.cs
@@ -7,6 +7,7 @@ public class MoveIt : MonoBehaviour
     public float probeDistance = 10f;
     private float nextTime = 0f;
     private Rigidbody rb;
+    private int counter = 0;
 
     void Start()
     {
@@ -15,13 +16,14 @@ public class MoveIt : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), probeDistance))
+        counter++;
+        if (counter % 8 == 0 && !Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), probeDistance))
             rb.AddRelativeForce(Vector3.forward * Mathf.Clamp(maxSpeed - Vector3.Magnitude(rb.velocity), -.1f * maxSpeed, maxSpeed) * Time.fixedDeltaTime);
 
         if (nextTime < Time.time)
         {
-			nextTime = Time.time + Random.Range(minTime, maxTime);
-			rb.AddRelativeTorque(Vector3.up * maxTorque * Random.Range(-1f, 1f));
+            nextTime = Time.time + Random.Range(minTime, maxTime);
+            rb.AddRelativeTorque(Vector3.up * maxTorque * Random.Range(-1f, 1f));
         }
     }
 }
