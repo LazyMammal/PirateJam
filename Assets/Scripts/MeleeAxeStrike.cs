d5b5c3205fac6d92ff51d28696e1b55f98f14f4e
Dennis Carlstrom <lazy@lazymammal.com>
2018-02-25 14:26:44 -0500

    swing batta batta

---
+++ Assets/Scripts/MeleeAxeStrike.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MeleeAxeStrike : MonoBehaviour
+{
+    public Transform axeIdle, axeSwing;
+	public float attackSpeed = 5f, returnSpeed = 3f;
+    private Quaternion rotIdle, rotSwing, rotForward, rotBack;
+	private float swingTime = 0f, returnTime = 0f;
+
+    void Start()
+    {
+        axeSwing.gameObject.SetActive(false);
+        rotIdle = axeIdle.localRotation;
+        rotSwing = axeSwing.localRotation;
+		rotBack = rotIdle;
+		rotForward = rotIdle;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+			rotBack = axeIdle.localRotation;
+			returnTime = 0f;
+			swingTime += Time.deltaTime;
+			axeIdle.localRotation = Quaternion.Slerp(rotForward, rotSwing, swingTime * attackSpeed);
+        }
+		else
+		{
+			rotForward = axeIdle.localRotation;
+			swingTime = 0f;
+			returnTime += Time.deltaTime;
+			axeIdle.localRotation = Quaternion.Slerp(rotBack, rotIdle, returnTime * returnSpeed * Mathf.Lerp(.5f, 1f, returnTime));
+		}
+    }
+}
