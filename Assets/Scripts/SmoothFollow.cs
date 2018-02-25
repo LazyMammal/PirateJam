e6b1f0a41ab1e7f14687dc69f7627aebdaedd4cf
Dennis Carlstrom <lazy@lazymammal.com>
2018-02-25 08:12:04 -0500

    many fish

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -6,14 +6,28 @@ using System.Collections;
 
 public class SmoothFollow : MonoBehaviour
 {
-    public Transform target;
+    public Transform targetParent;
+    private Transform target;
     public float distance = 10.0f;
     public float height = 5.0f;
     public float heightDamping = 2.0f;
     public float rotationDamping = 3.0f;
+    public float minTime = 1.5f, maxTime = 3.5f;
+    private float nextTime = 0f;
+
+
     void LateUpdate()
     {
-        if (!target) return;
+        float hDamp = heightDamping;
+        float rDamp = rotationDamping;
+
+        if (!target || nextTime < Time.time)
+        {
+			nextTime = Time.time + Random.Range(minTime, maxTime);
+            target = targetParent.GetChild(Random.Range(0, targetParent.childCount));
+            hDamp = 1.0f / Time.deltaTime;
+            rDamp = hDamp;
+        }
 
         // Calculate the current rotation angles
         float wantedRotationAngle = target.eulerAngles.y;
@@ -22,24 +36,15 @@ public class SmoothFollow : MonoBehaviour
         float currentRotationAngle = transform.eulerAngles.y;
         float currentHeight = transform.position.y;
 
-        // Damp the rotation around the y-axis
-        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rDamp * Time.deltaTime);
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, hDamp * Time.deltaTime);
 
-        // Damp the height
-        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
-
-        // Convert the angle into a rotation
         var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
 
-        // Set the position of the camera on the x-z plane to:
-        // distance meters behind the target
         transform.position = target.position;
         transform.position -= currentRotation * Vector3.forward * distance;
 
-        // Set the height of the camera
         transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
-
-        // Always look at the target
         transform.LookAt(target);
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public Collider[] spawnRegions;
+    public Transform objParent = null;
+
+    public int maxPopulation = 10;
+    void Start()
+    {
+        for (int i = 0; i < maxPopulation; i++)
+        {
+            GameObject prefab = prefabs[Random.Range(0, prefabs.Length)];
+            Collider spawnRegion = spawnRegions[Random.Range(0, spawnRegions.Length)].GetComponent<Collider>();
+
+            Vector3 pos = new Vector3(
+                Random.Range(spawnRegion.bounds.min.x, spawnRegion.bounds.max.x),
+                0f,
+                Random.Range(spawnRegion.bounds.min.z, spawnRegion.bounds.max.z)
+            );
+            pos.y = Terrain.activeTerrain.transform.TransformPoint(0f, Terrain.activeTerrain.SampleHeight(pos), 0f).y;
+
+            if (pos.y < spawnRegion.bounds.max.y)
+            {
+                pos.y = Random.Range(pos.y, spawnRegion.bounds.max.y);
+                GameObject go = Instantiate(prefab, pos, Quaternion.identity);
+                go.transform.parent = objParent ? objParent : transform;
+            }
+        }
+    }
+}
