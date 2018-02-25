5358c96a081cb5a4a2b348d82b3bcddfdb4c88f3
Dennis Carlstrom <lazy@lazymammal.com>
2018-02-25 14:51:19 -0500

    stunning attack

---
+++ Assets/Scripts/TakeDamage.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class TakeDamage : MonoBehaviour
+{
+    public int hitPoints = 5;
+    public string tagName = "HeroWeapon";
+    private NavMeshAgent agent;
+    private Animator anim;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == tagName)
+        {
+			agent.ResetPath();
+            anim.SetBool("move", false);
+            hitPoints -= 1;
+
+            if (hitPoints > 0)
+            {
+                anim.SetTrigger("damage");
+            }
+            else
+            {
+                anim.SetTrigger("pushback");
+                // TODO: fall to pieces
+            }
+        }
+    }
+}
