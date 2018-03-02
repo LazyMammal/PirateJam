c53c70460c25b9d13f417229a9dbf71ead02bd77
Dennis Carlstrom <lazy@lazymammal.com>
2018-03-02 15:08:28 -0500

    hit sound

---
+++ Assets/Scripts/MeleeHitSound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioClip))]
+public class MeleeHitSound : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+		if (Input.GetButton("Fire1"))
+			GetComponent<AudioSource>().Play();
+    }
+}
