a3da5caa66f40e31d02be939d0f324167f3daacf
Dennis Carlstrom <lazy@lazymammal.com>
2018-03-02 14:26:24 -0500

    menu working

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,8 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class GameController : MonoBehaviour
 {
     public Camera mainCam, fishCam;
+    public Transform menuPanel, playButton, resumeButton, restartButton, player, skeletonSpawner;
+    private bool isPaused = true, arcadeMode = true;
+    private MouseLook mouseLook;
+
+    void Start()
+    {
+        Time.timeScale = 0f;
+        restartButton.gameObject.SetActive(false);
+        resumeButton.gameObject.SetActive(false);
+        mouseLook = player.GetComponentInChildren<RigidbodyFirstPersonController>().mouseLook;
+        mouseLook.SetCursorLock(!isPaused);
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -12,19 +27,43 @@ public class GameController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.F2))
         {
-            SwitchCamera();
+            arcadeMode = !arcadeMode;
+            UpdateMode();
         }
     }
 
-    public void SwitchCamera()
+    public void WatchDaFishy()
     {
-        mainCam.gameObject.SetActive(false);
-        fishCam.gameObject.SetActive(true);
+        arcadeMode = false;
+        UpdateMode();
+        if(isPaused)
+            PauseGame();
+    }
+
+    public void UpdateMode()
+    {
+        skeletonSpawner.gameObject.SetActive(arcadeMode);
+        mainCam.gameObject.SetActive(arcadeMode);
+        fishCam.gameObject.SetActive(!arcadeMode);
     }
 
     public void PauseGame()
     {
+        isPaused = !isPaused;
+
+        menuPanel.gameObject.SetActive(isPaused);
+        playButton.gameObject.SetActive(false);
+        resumeButton.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+
+        mouseLook.SetCursorLock(!isPaused);
         Time.timeScale = 1f - Time.timeScale;
+    }
+
+    public void RestartGame()
+    {
+        Scene loadedLevel = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(loadedLevel.buildIndex);
     }
 
     public void QuitGame()
